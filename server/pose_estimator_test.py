946c8e8a128f63176baa5c70598321f2fd7448ce
Ravindu Senarathna <80905394+MrRevengerX@users.noreply.github.com>
2023-04-04 13:59:38 +0530

    Added ci/cd test cases.

---
+++ server/pose_estimator_test.py
@@ -0,0 +1,22 @@
+import pytest
+import pickle
+import os.path
+
+
+def test_load_model():
+    with open('shoulder_press.pkl', 'rb') as f:
+        model = pickle.load(f)
+    assert model is not None
+
+# If uploads folder is not empty, fail the test
+def test_read_video_file():
+    assert len(os.listdir('uploads')) == 0
+
+# If the result folder is not empty, fail the test
+def test_write_video_file():
+    assert len(os.listdir('result')) == 0
+
+
+
+
+
