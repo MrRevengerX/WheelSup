2855dcf2d4956adc0fd3de0e199caa4137dad2a2
Ravindu Senarathna <80905394+MrRevengerX@users.noreply.github.com>
2023-04-06 05:10:02 +0530

    Trying adding ssl to server.

--- server/server.py
+++ server/server.py
@@ -2,9 +2,14 @@ import subprocess
 from flask import Flask, request, jsonify,send_file
 import os
 from flask_cors import CORS
+from OpenSSL import SSL
 
 app = Flask(__name__)
 CORS(app)
+
+# Define SSL context
+context = SSL.Context(SSL.PROTOCOL_TLSv1_2)
+context.load_cert_chain('cetfile.pem', 'key.pem')
 
 @app.route('/', methods=['GET'])
 def index():
@@ -76,4 +81,4 @@ def delete_files():
     return 'Files deleted successfully', 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, ssl_context=context)
