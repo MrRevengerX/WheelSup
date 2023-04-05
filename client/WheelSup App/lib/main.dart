21df569c02c5d3094be06654adc0689d02d3bb93
Janidu Rathnayaka <86180073+janiidu14@users.noreply.github.com>
2023-04-05 17:56:24 +0530

    Added Delete Method to Application

--- client/WheelSup App/lib/main.dart
+++ client/WheelSup App/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
-import 'package:summary_page/screens/SummaryPage.dart';
 import 'screens/WelcomePage.dart';
 
 void main() => runApp(
