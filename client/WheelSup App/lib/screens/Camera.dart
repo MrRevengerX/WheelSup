249635ac4232500c5bfdd9cf76ecd74d280c9613
Janidu Rathnayaka <86180073+janiidu14@users.noreply.github.com>
2023-04-05 18:26:11 +0530

    Updated Camera Page

---
+++ client/WheelSup App/lib/screens/Camera.dart
@@ -0,0 +1,52 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+
+List<CameraDescription> cameras = [];
+
+class Camera extends StatefulWidget {
+  const Camera({super.key});
+
+  @override
+  _CameraState createState() => _CameraState();
+}
+
+class _CameraState extends State<Camera> {
+  late CameraController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    availableCameras().then((availableCameras) {
+      cameras = availableCameras;
+      controller = CameraController(cameras[0], ResolutionPreset.medium);
+      controller.initialize().then((_) {
+        if (!mounted) {
+          return;
+        }
+        setState(() {});
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!controller.value.isInitialized) {
+      return Container();
+    }
+    return Scaffold(
+      body: CameraPreview(controller),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.videocam),
+      ),
+    );
+  }
+}
